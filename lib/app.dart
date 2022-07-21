b85c26ea5b06667bc885f446fe997a29e728353f
sakmongkhon018 <aucss02@gmail.com>
2022-07-21 17:36:25 +0700

    up

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_week10/image_deom.dart';
 
+import 'form_demo.dart';
+
 
 class MyApp extends StatelessWidget{
   const MyApp({Key? key}):super(key:key);//1.constrctor
@@ -8,8 +10,9 @@ class MyApp extends StatelessWidget{
   @override //2.build
   Widget build(BuildContext context){
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Flutter App",
-      home: ImageDemo(
+      home: FormDemo(
 
       ),
     );
